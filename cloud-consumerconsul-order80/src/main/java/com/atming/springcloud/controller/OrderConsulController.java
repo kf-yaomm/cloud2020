6167bc1953146c6e3d119ac281c092bb3cc1c555
yaomingming <yaomingming1103@163.com>
2021-06-02 09:00:08 +0800

    支付微服务集群配置 @LoadBalanced默认轮询

--- cloud-consumerconsul-order80/src/main/java/com/atming/springcloud/controller/OrderConsulController.java
+++ cloud-consumerconsul-order80/src/main/java/com/atming/springcloud/controller/OrderConsulController.java
@@ -18,14 +18,16 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 public class OrderConsulController {
-    public static final String INVOKE_URL = "http://localhost:8001/";
+   // public static final String INVOKE_URL = "http://localhost:8001";
+    public static final String INVOKE_URL = "http://cloud-payment-service";
+
     @Resource
     private RestTemplate restTemplate;
 
     @GetMapping(value = "/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment ) {
 
-        return restTemplate.postForObject(INVOKE_URL+"payment/create", payment, CommonResult.class);
+        return restTemplate.postForObject(INVOKE_URL+"/payment/create", payment, CommonResult.class);
     }
 
 
@@ -33,7 +35,7 @@ public class OrderConsulController {
     @GetMapping(value = "/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id ) {
 
-        return restTemplate.getForObject(INVOKE_URL+"payment/get/"+id, CommonResult.class);
+        return restTemplate.getForObject(INVOKE_URL+"/payment/get/"+id, CommonResult.class);
     }
 
 
