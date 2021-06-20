5b9845a80aa457b5989eb455442e28dd0a445df5
yaomingming <yaomingming1103@163.com>
2021-06-20 12:26:47 +0800

    gateway路由和断言

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+
+
+    public static void main(String[] args) {
+
+        ZonedDateTime zbj=ZonedDateTime.now();
+        System.out.println(zbj);
+
+        //2021-06-20T11:33:17.110+08:00[Asia/Shanghai]
+    }
+}
