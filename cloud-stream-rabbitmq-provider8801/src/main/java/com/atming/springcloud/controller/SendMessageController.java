6a89f7ddcc9c0d6af495a8a77d89204dbbac5fe2
yaomingming <yaomingming1103@163.com>
2021-06-29 22:59:34 +0800

    stream消息驱动之生产者构建over

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atming/springcloud/controller/SendMessageController.java
@@ -0,0 +1,24 @@
+package com.atming.springcloud.controller;
+
+import com.atming.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Yang Hao
+ * @description
+ * @date 2020-09-21 15:33
+ */
+@RestController
+public class SendMessageController {
+    @Resource
+    private IMessageProvider iMessageProvider;
+
+    @GetMapping("/sendMessage")
+    public String sendMessage() {
+        return iMessageProvider.send();
+    }
+
+}
