e0838fed16e1a878d0a2b9508c269b633aa14968
yaomingming <yaomingming1103@163.com>
2021-07-07 22:55:19 +0800

    热点限流与例外项

--- cloudalibaba-sentinel-service8401/src/main/java/com/atming/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atming/springcloud/controller/FlowLimitController.java
@@ -36,12 +36,12 @@ public class FlowLimitController {
 
     @GetMapping("/testD")
     public String testD() {
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-//        log.info("testD 测试RT");
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        log.info("testD 测试RT");
         log.info("testD 异常比例");
         int age = 10 / 0;
         return "------testD";
@@ -55,13 +55,18 @@ public class FlowLimitController {
         return "------testE 测试异常数";
     }
 
+    //热点限流
     @GetMapping("/testHotKey")
     @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
     public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
                              @RequestParam(value = "p2", required = false) String p2) {
+
+       //int i=10/0; 不会兜底的 走异常
+
         return "-------testHotKey";
     }
 
+    //兜底
     public String deal_testHotKey(String p1, String p2,
                                   BlockException exception) {
         return "----deal_testHotKey,o(╥﹏╥)o";
