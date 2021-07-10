d22bdbcb0108624cd7a37db2b1d1fe5d6ea814b2
yaomingming <yaomingming1103@163.com>
2021-07-10 18:44:26 +0800

    seata分布式事务-完结

--- seata-account-service2003/src/main/java/com/atming/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ seata-account-service2003/src/main/java/com/atming/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 账户业务实现类
@@ -27,11 +28,13 @@ public class AccountServiceImpl implements AccountService {
         //  模拟异常 配合 2001 服务里面的注解    @GlobalTransactional(name = "fsp-create-order", rollbackFor = Exception.class)
         //  导致结果,订单状态没变成已完成,库存和账户都被扣减
         //  正常测试注销try catch.
+        //为啥是20s openFeign默认是1S
 //        try {
 //            TimeUnit.SECONDS.sleep(20);
 //        } catch (InterruptedException e) {
 //            e.printStackTrace();
 //        }
+
         accountDao.decrease(userId, money);
         log.info("------------->account-service 中扣减库存结束");
     }
--- seata-order-service2001/src/main/java/com/atming/springcloud/alibaba/service/impl/OrderServiceImpl.java
+++ seata-order-service2001/src/main/java/com/atming/springcloud/alibaba/service/impl/OrderServiceImpl.java
@@ -33,6 +33,7 @@ public class OrderServiceImpl implements OrderService {
      * @param order
      */
     @Override
+    @GlobalTransactional(name = "yaomm-create-order_dsfgfdg", rollbackFor = Exception.class)
     public void create(Order order) {
         log.info("------------->开始新建订单");
         orderDao.create(order);
